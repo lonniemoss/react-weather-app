d7843cc6b3b4a103d136926ab7f78808072249cc
lonniemoss <gloryjacques1@gmail.com>
2023-04-30 12:23:20 -0400

    done with search + css a bit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Search from './components/search/search';
-import CurrentWeather from './components/current-weather/current-weather';
 
 function App() {
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <div className='container'>
       <Search onSearchChange={handleOnSearchChange} />
-      <CurrentWeather />
     </div>
   );
 }
--- src/api 2.js
+++
@@ -1,9 +0,0 @@
-export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '8bd5f0a630msh3cfe02c852da299p14bfdbjsncdcf79123245',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
